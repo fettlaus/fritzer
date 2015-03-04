19c1d6f25d8fc0f2f5300678e8747836fbe21f93
Arne Wischer <Fettlaus@gmail.com>
2015-03-04 15:20:14 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Fritzer',
+      version='0.0.1',
+      description='Unblock-US Updater by FritzBox IP',
+      author='Fettlaus',
+      author_email='Fettlaus@gmail.com',
+      url='https://projects.fettlaus.de/python/fritzer',
+      packages=['fritzer'],
+      scripts=['fritzer/fritzer'],
+     )
